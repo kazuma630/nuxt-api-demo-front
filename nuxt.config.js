593664721e9c633ace20008b5ee4b03d3eb15830
maekawa-kazuma <kazumaekawa7218@gmail.com>
2022-03-20 20:12:43 +0900

    axiosの初期設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    'plugins/axios'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -50,5 +51,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    // build高速化のため
+    hardSource: true
   }
 }
