b2db912b4ef68da36712079964847a6a5bbcd8b0
maekawa-kazuma <kazumaekawa7218@gmail.com>
2022-04-03 21:56:48 +0900

    Vuex導入・scroll機能作成

---
+++ components/AppLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-avatar
+    color="black"
+    size="40"
+    class="my-app-logo"
+  >
+    <span class="white--text text-subtitle-2">
+      Nuxt
+    </span>
+  </v-avatar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+  .my-app-logo {
+    margin-right: 8px;
+    cursor: pointer;
+  }
+</style>
--- components/Home/HomeAppBar.vue
+++ components/Home/HomeAppBar.vue
@@ -1,9 +1,73 @@
 <template>
-  <div>
-    HomeAppBar.vue
-  </div>
+  <v-app-bar
+    app
+    :dark="!isScrollPoint"
+    :height="homeAppBarHeight"
+    :color="toolbarStyle.color"
+    :elevation="toolbarStyle.elevation"
+  >
+    <app-logo
+      @click.native="$vuetify.goTo('#scroll-top')"
+    />
+    <v-toolbar-title>
+      {{ appName }}
+    </v-toolbar-title>
+    <!-- v-spacer: このタグ以下のHTML要素を右寄せにする。 -->
+    <v-spacer />
+    <v-toolbar-items class="ml-2">
+      <v-btn
+        v-for="(menu, i) in menus"
+        :key="`menu-btn-${i}`"
+        text
+        @click="$vuetify.goTo(`#${menu.title}`)"
+      >
+        {{ $t(`menus.${menu.title}`) }}
+      </v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
 </template>
 
 <script>
-export default {}
+import AppLogo from '../AppLogo.vue'
+export default {
+  components: { AppLogo },
+  props: {
+    menus: {
+      type: Array,
+      default: () => []
+    },
+    imgHeight: {
+      type: Number,
+      default: 0
+    }
+  },
+  data ({ $config: { appName }, $store }) {
+    return {
+      appName,
+      scrollY: 0,
+      homeAppBarHeight: $store.state.styles.homeAppBarHeight
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  computed: {
+    isScrollPoint () {
+      return this.scrollY > (this.imgHeight - this.homeAppBarHeight)
+    },
+    toolbarStyle () {
+      const color = this.isScrollPoint ? 'white' : 'transparent'
+      const elevation = this.isScrollPoint ? 4 : 0
+      return { color, elevation }
+    }
+  },
+  methods: {
+    onScroll () {
+      this.scrollY = window.scrollY
+    }
+  }
+}
 </script>
